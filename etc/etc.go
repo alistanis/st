9c88e6d29b52021666e25c507ca7071bba4ab7a9
Chris Cooper <ccooper@sessiom.com>
2016-01-20 17:29:09 -0500

    don't tag unexported fields

--- etc/etc.go
+++ etc/etc.go
@@ -56,3 +56,8 @@ type TestStructWithTagsCamel struct {
 	MapStringInt    map[string]int    `json:"MapStringInt"`
 	MapIntString    map[int]string    `json:"MapIntString"`
 }
+
+type TestUnexported struct {
+	testUnexportedInt int
+	TestExportedInt   int
+}
