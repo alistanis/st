3a28dffa7be3f580800389771233c9b5a1236edd
Chris Cooper <ccooper@sessionm.com>
2016-01-21 01:33:15 -0500

    fix more misspellings

--- sterrors/sterrors.go
+++ sterrors/sterrors.go
@@ -10,7 +10,7 @@ func MutuallyExclusiveParameters(p, p2 string) error {
 	return fmt.Errorf("Mutually exclusive parameters provided: %s and %s", p, p2)
 }
 
-// Print a string depending on verbosity
+// Print a string depending on verbosity... should be in a debug package?
 func Printf(s string, args ...interface{}) {
 	if Verbose {
 		fmt.Printf(s, args...)
