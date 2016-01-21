1f3615c4b43d9e8fd70e8a78fa0aef3d9936c01d
Chris Cooper <ccooper@sessionm.com>
2016-01-21 01:22:06 -0500

    allow structs to be ignored

--- flags/flags.go
+++ flags/flags.go
@@ -12,13 +12,16 @@ var (
 	Case string
 	Tag  string
 
-	Append              bool
-	c                   bool
-	s                   bool
-	Verbose             bool
-	Write               bool
-	IgnoredFields       []string
-	IgnoredFieldsString string
+	Append               bool
+	Overwrite            bool
+	c                    bool
+	s                    bool
+	Verbose              bool
+	Write                bool
+	IgnoredFields        []string
+	IgnoredFieldsString  string
+	IgnoredStructs       []string
+	IgnoredStructsString string
 )
 
 const (
@@ -28,15 +31,18 @@ const (
 
 func StringVars() {
 	flag.StringVar(&Tag, "t", "json", "The struct tag to use when tagging. Example: -t=json ")
-	flag.StringVar(&IgnoredFieldsString, "i", "", "A comma separated list of strings to ignore")
+	flag.StringVar(&IgnoredFieldsString, "i", "", "A comma separated list of fields to ignore. Will use the format json:\"-\".")
+	flag.StringVar(&IgnoredStructsString, "is", "", "A comma separated list of structs to ignore. Will not tag any fields in the struct.")
+
 }
 
 func BoolVars() {
-	flag.BoolVar(&c, "c", false, "Sets the struct tag to camel case")
-	flag.BoolVar(&s, "s", false, "Sets the struct tag to snake case")
-	flag.BoolVar(&Append, "a", false, "Sets mode to append mode. The default is to overwrite existing tags.")
+	flag.BoolVar(&c, "c", false, "Sets the struct tag to camel case.")
+	flag.BoolVar(&s, "s", false, "Sets the struct tag to snake case.")
+	flag.BoolVar(&Append, "a", false, "Sets mode to append mode. Will append to existing tags. Default behavior skips existing tags.")
 	flag.BoolVar(&Verbose, "v", false, "Sets mode to verbose.")
 	flag.BoolVar(&Write, "w", false, "Sets mode to write to source file.")
+	flag.BoolVar(&Overwrite, "o", false, "Sets mode to overwrite mode. Will overwrite existing tags (completely). Default behavior skips existing tags.")
 }
 
 func SetVars() {
@@ -59,6 +65,10 @@ func verify() error {
 		return sterrors.MutuallyExclusiveParameters("c", "s")
 	}
 
+	if Overwrite && Append {
+		return sterrors.MutuallyExclusiveParameters("o", "a")
+	}
+
 	if c {
 		Case = Camel
 	}
@@ -70,6 +80,10 @@ func verify() error {
 
 	if IgnoredFieldsString != "" {
 		IgnoredFields = strings.Split(IgnoredFieldsString, ",")
+	}
+
+	if IgnoredStructsString != "" {
+		IgnoredStructs = strings.Split(IgnoredStructsString, ",")
 	}
 	return nil
 }
