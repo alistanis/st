88fc07abd23b5c30d835d2ba4c732f63edbc9792
Chris Cooper <ccooper@sessionm.com>
2016-01-25 11:44:25 -0500

    adds test data

---
+++ parse/parse_test.go
@@ -0,0 +1,60 @@
+package parse
+
+import "strings"
+
+var (
+	testDataNoExistingTags = `type TestStruct struct {
+	Int             int
+	Int64           int64
+	IntSlice        []int
+	Int64Slice      []int64
+	String          string
+	StringSlice     []string
+	Float           float64
+	FloatSlice      []float64
+	UIntPointer     uintptr
+	Rune            rune
+	RuneSlice       []rune
+	Byte            byte
+	ByteSlice       []byte
+	MapStringString map[string]string
+	MapStringInt    map[string]int
+	MapIntString    map[int]string
+}`
+	snakeTestDataExistingTags = strings.Replace(`type TestStructWithTagsSnake struct {
+	Int             int               %sjson:"int"%s
+	Int64           int64             %sjson:"int_64"%s
+	IntSlice        []int             %sjson:"int_slice"%s
+	Int64Slice      []int64           %sjson:"int_64_slice"%s
+	String          string            %sjson:"string"%s
+	StringSlice     []string          %sjson:"string_slice"%s
+	Float           float64           %sjson:"float"%s
+	FloatSlice      []float64         %sjson:"float_slice"%s
+	UIntPointer     uintptr           %sjson:"u_int_pointer"%s
+	Rune            rune              %sjson:"rune"%s
+	RuneSlice       []rune            %sjson:"rune_slice"%s
+	Byte            byte              %sjson:"byte"%s
+	ByteSlice       []byte            %sjson:"byte_slice"%s
+	MapStringString map[string]string %sjson:"map_string_string"%s
+	MapStringInt    map[string]int    %sjson:"map_string_int"%s
+	MapIntString    map[int]string    %sjson:"map_int_string"%s
+}`, "%s", "`", -1)
+	camelTestDataExistingTags = strings.Replace(`type TestStructWithTagsCamel struct {
+	Int             int               %sjson:"Int"%s
+	Int64           int64             %sjson:"Int64"%s
+	IntSlice        []int             %sjson:"IntSlice"%s
+	Int64Slice      []int64           %sjson:"Int64Slice"%s
+	String          string            %sjson:"String"%s
+	StringSlice     []string          %sjson:"StringSlice"%s
+	Float           float64           %sjson:"Float"%s
+	FloatSlice      []float64         %sjson:"FloatSlice"%s
+	UIntPointer     uintptr           %sjson:"UIntPointer"%s
+	Rune            rune              %sjson:"Rune"%s
+	RuneSlice       []rune            %sjson:"RuneSlice"%s
+	Byte            byte              %sjson:"Byte"%s
+	ByteSlice       []byte            %sjson:"ByteSlice"%s
+	MapStringString map[string]string %sjson:"MapStringString"%s
+	MapStringInt    map[string]int    %sjson:"MapStringInt"%s
+	MapIntString    map[int]string    %sjson:"MapIntString"%s
+}`, "%s", "`", -1)
+)
